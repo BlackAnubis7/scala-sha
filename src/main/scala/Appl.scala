55d4b0cf13d955361ec71e78f28e51af537bbee3
BlackAnubis7 <marcin.k.fac@gmail.com>
2021-05-05 20:06:59 +0200

    unnecessary libraries removed

--- src/main/scala/Appl.scala
+++ src/main/scala/Appl.scala
@@ -1,18 +1,9 @@
-import java.io.File
-import java.nio.file.{Files, Path}
-import java.io.IOException
-import java.nio.file.attribute.FileTime
-import java.util.concurrent.TimeUnit
-
-import javax.swing.JFileChooser
-
 // ask:
 // - outscoping variables
 // - when use types
 
 object Appl {
     def main(args: Array[String]): Unit = {
-        println(System.getProperty("user.dir") + "\\src\\test")
-        //FileChooser.choose()
+        FileChooser.choose()
     }
 }
